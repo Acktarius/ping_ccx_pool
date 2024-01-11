b222a2147212c0263017b92f548ee5e9e8b5bc08
Acktarius <94026250+Acktarius@users.noreply.github.com>
2024-01-11 13:20:15 +0000

    Update ping_ccx_pool.sh (#1)

--- ping_ccx_pool.sh
+++ ping_ccx_pool.sh
@@ -20,89 +20,92 @@ case "$TERM" in
         TURNOFF=''
         ;;
 esac
-
 #Presentation
+presentation (){
 clear
 echo -e "${GRIS}####################################################################"
 echo -e "#                                                                  #"
-echo -e "${GRIS}###   ${WHITE}          TEST CCX POOLS AVERAGE RESPONSE TIME${TURNOFF}${GRIS}             ###"
+echo -e "${GRIS}###   ${WHITE}          TEST CCX POOLS AVERAGE RESPONSE TIME${TURNOFF}${GRIS}               #"
 echo -e "#                                                                  #"
 echo -e "#                                                                  #"
-echo -e "####################################################    .::::."
-echo -e "#                                                   .:---=--=--::."
-echo -e "#${WHITE} Select the pools you 'd like to test,${TURNOFF}${GRIS}\t\t    -=:+-.  .-=:=:"
-echo -e "# 					\t    -=:+."
-echo -e "# 					\t    -=:+."
-echo -e "#                                                   -=:+."
-echo -e "#						    -=:=."
-echo -e "#                                                   -+:-:    .::."
-echo -e "#						    -+==------===-"
-echo -e "####################################################   :-=-==-:${TURNOFF}\n"
-
+echo -e "####################################################    ${WHITE}.::::."
+echo -e "${GRIS}#                                                   ${WHITE}.:---=--=--::."
+echo -e "#${WHITE} Select the pools you 'd like to test,\t\t    -=:+-.  .-=:=:"
+echo -e "${GRIS}# 					\t    ${WHITE}-=:+."
+echo -e "${GRIS}# 					\t    ${WHITE}-=:+."
+echo -e "${GRIS}#                                                   ${WHITE}-=:+."
+echo -e "${GRIS}#						    ${WHITE}-=:=."
+echo -e "${GRIS}#                                                   ${WHITE}-+:-:    .::."
+echo -e "${GRIS}#						    ${WHITE}-+==------===-"
+echo -e "${GRIS}####################################################   ${WHITE}:-=-==-:${TURNOFF}\n"
+}
 #Test nping available
-if [[ ! -f /bin/nping ]]; then
+if ! command -v nping &> /dev/null; then
 echo -e "${ORANGE}NPING is needed for this script${GRIS}\nto install it: ${WHITE}sudo apt install nmap${TURNOFF}\n"
 exit
 fi
 #Test zenity available
-if [[ ! -f /bin/zenity ]]; then
+if ! command -v zenity &> /dev/null; then
 echo -e "${ORANGE}Zenity is needed for this script${GRIS}\nto install it: ${WHITE}sudo apt install zenity${TURNOFF}\n"
 exit
 fi
 
 #pools list
-pools=("pool.conceal.network" "3333" "conceal.cedric-crispin.com" "3364" "ccx.gntl.uk" "10012" "us.conceal.herominers.com" "1115" "pool.hashvault.pro" "3333")
+declare -A poolAndPort=(
+[1,0]="pool.conceal.network" [1,1]="3333" \
+[2,0]="conceal.cedric-crispin.com" [2,1]="3364" \
+[3,0]="us.fastpool.xyz" [3,1]="10167" \
+[4,0]="ccx.gntl.uk" [4,1]="40012" \
+[5,0]="us.conceal.herominers.com" [5,1]="1115" \
+[6,0]="pool.hashvault.pro" [6,1]="3333")
+
 #ping pool function
 ping_pool () {
-artcc=$(nping --tcp -p ${pools[ $(( $1 + 1 )) ]} -c 5 ${pools[$1]} | grep "Avg rtt" | cut -d ":" -f 4 | tr -s " " | cut -d "." -f 1 | xargs)
+artcc=$(nping --tcp -p ${poolAndPort["$1",1]} -c 5 ${poolAndPort["$1",0]} | grep "Avg rtt" | cut -d ":" -f 4 | tr -s " " | cut -d "." -f 1 | xargs)
 echo "$artcc"
 }
 
+presentation
 #list
 sleep 1
-list=$(zenity --list --checklist --height 320 --width 400 --title "Pools you want to ping" --column "Select" --column "#" --column "Pool" --column "port" \
+list=$(zenity --list --checklist --height 320 --width 400 --title "Pools you want to ping" --timeout 25 --column "Select" --column "#" --column "Pool" --column "port" \
 FALSE 1 pool.conceal.network 3333 \
 FALSE 2 conceal.cedric-crispin.com 3364 \
-FALSE 3 ccx.gntl.uk 10012 \
-FALSE 4 us.conceal.herominers.com 1115 \
-FALSE 5 pool.hashvault.pro 3333 \
-FALSE 6 other xxxx \
+FALSE 3 us.fastpool.xyz 10167 \
+FALSE 4 ccx.gntl.uk xxxxx \
+FALSE 5 us.conceal.herominers.com 1115 \
+FALSE 6 pool.hashvault.pro 3333 \
+FALSE 7 other xxxx \
 )
-
+list=$(echo "$list" | tr "|" " ")
 #test if nothing has been done
-if [[ $? -eq 1 ]] || [[ $list -eq "" ]]; then
+if [[ $? -eq 1 ]] || [[ -z $list ]]; then
 echo "no pool selected"
 sleep 1
 clear
 exit
 fi
 #check if other has been checked and add it to pools as needed
-last=$(( ${#list} - 1 ))
-if [[ "${list:$last:1}" = "6" ]]; then
+if [[ "${list:(-1)}" = "7" ]]; then
 extrapool=$(zenity --forms --separator " " --height 320 --width 400 --text "other pool" --add-entry "Pool address" --add-entry "port")
 xpool=$(cut -d " " -f 1 <<< $extrapool)
 xport=$(cut -d " " -f 2 <<< $extrapool)
-if [[ $xpool != *"."* ]] || [[ ! $xport =~ ^-?[0-9]+$ ]]; then
-zenity --error --height 320 --width 400 --title Error --text "pool address doesn't seems right"
-sleep 2
-clear
-exit
-else
-		pools[${#pools[@]}]=$xpool
-		pools[${#pools[@]}]=$xport
-fi
+        if [[ $xpool != *"."* ]] || [[ ! $xport =~ ^-?[0-9]+$ ]]; then
+        zenity --error --height 320 --width 400 --title Error --text "pool address doesn't seems right"
+        sleep 2
+        clear
+        exit
+        else
+	poolAndPort[7,0]=$xpool
+	poolAndPort[7,1]=$xport
+        fi
 fi
 
 #Pinging
-declare -a results
-i=0
-while [[ i -le ${#list} ]]
-do
-x=$(( 2 * ${list:$i:1} - 2 ))
-echo -ne "${GRIS}# running test for ${ORANGE}${pools[$x]}${TURNOFF}\033[0K\r"
-results+=($(ping_pool $x)":"${pools[$x]})
+for i in ${list}; do
+echo -ne "${GRIS}# running nping for ${ORANGE}${poolAndPort["$i",0]}${TURNOFF}\033[0K\r"
+poolAndPort["$i",2]="$(ping_pool $i)"
 sleep 1
-i=$(( i+2 ))
 done
 unset i
 
@@ -113,11 +116,8 @@ echo -e "${GRIS}#${WHITE}           Average response time for the pools selected
 echo -e "#                                                                  #"
 #Sorting
 (
-for k in "${!results[@]}"
-do
-R=$(echo "${results[$k]}" | cut -d ":" -f 2)
-T=$(echo "${results[$k]}" | cut -d ":" -f 1)
-echo -e "\t$T ms \t\t$R"
+for k in ${list}; do
+echo -e "#\t${poolAndPort["$k",2]} ms \t\t${poolAndPort["$k",0]}"
 done
 ) | sort -t$'\t' -k2 -n
 echo -e "#                                                                  #"
